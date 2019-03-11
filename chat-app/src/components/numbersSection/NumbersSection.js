e21751c2b323c94de902986e8ef1218eec1052fb
Damian-Czarnota <yziek@windowslive.com>
2019-03-11 21:18:50 +0100

    Adds NumbersSection component

---
+++ chat-app/src/components/numbersSection/NumbersSection.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Counter from '../../utils/Counter/Counter';
+
+
+export default class NumbersSection extends Component {
+
+    render() {
+        return(
+            <div>
+                
+            </div>
+        )
+    }
+
+}
