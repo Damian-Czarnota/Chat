ae5907e33a0134b8d711091f5c60e29541edd9fd
Damian-Czarnota <yziek@windowslive.com>
2019-03-15 13:44:45 +0100

    Adds new styles and changes background of Register component

--- chat-app/src/components/register/Register.js
+++ chat-app/src/components/register/Register.js
@@ -12,16 +12,26 @@ export default class Register extends Component {
                 <div className="section section--register">
                     <div className="wrapper">
                         <div className="section__boxes">
-                            <div className="box box--2">
-                                <h2>TREST</h2>
-                            </div>
                             <div className="box box--2 box--fill">
+                                <div className="box__wrapper box__wrapper--left">
+                                    <h2 className="section__title">Title</h2>
+                                    <p className="section__text">
+                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis vel tempor libero. Nam convallis mollis massa.
+                                        Duis tempor, odio vitae pulvinar maximus, arcu justo scelerisque purus, quis interdum elit sapien quis tortor.
+                                        Nullam vel magna vel orci malesuada convallis nec sit amet justo. Etiam at hendrerit risus, sed ultricies ipsum.
+                                        Pellentesque in elit id magna sollicitudin imperdiet ac et enim. Cras tincidunt eget tortor id vehicula.
+                                    </p>
+                                </div>
+                                <div className="wrapper__background">
+                                    <HexBackground />
+                                </div>
+                            </div>
+                            <div className="box box--2">
+                                <div className="box__wrapper">
 
+                                </div>
                             </div>
                         </div>
-                    </div>
-                    <div className="wrapper__background">
-                        <HexBackground />
                     </div>
                 </div>
         )
--- chat-app/src/components/whyUs/WhyUs.js
+++ chat-app/src/components/whyUs/WhyUs.js
@@ -11,7 +11,7 @@ export default class WhyUs extends Component {
                 <div className="wrapper">
                     <div className="section__description">
                         <h2 className="section__title">Why us?</h2>
-                        <p className="section__text">
+                        <p className="section__text section__text--center">
                             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis vel tempor libero. Nam convallis mollis massa.
                             Duis tempor, odio vitae pulvinar maximus, arcu justo scelerisque purus, quis interdum elit sapien quis tortor.
                             Nullam vel magna vel orci malesuada convallis nec sit amet justo. Etiam at hendrerit risus, sed ultricies ipsum.
